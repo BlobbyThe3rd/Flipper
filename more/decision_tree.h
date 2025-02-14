4449aa4a4da3f0196916476d33d13fa6d23e39e2
Gabe <131726918+BlobbyThe3rd@users.noreply.github.com>
2025-02-14 00:07:02 -0600

    Create decision_tree.h

---
+++ more/decision_tree.h
@@ -0,0 +1,10 @@
+// decision_tree.h
+#ifndef DECISION_TREE_H
+#define DECISION_TREE_H
+
+#include <stdint.h>
+
+// Decision tree function
+int decision_tree(float feature1, float feature2);
+
+#endif
